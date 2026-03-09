c575bc6cf47ce42b138f87c93ba88528f8b3949b
Elegance239 <elegancework239@gmail.com>
2026-03-09 12:28:49 +0800

    Fixed Deployment Bug

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -2,7 +2,6 @@
 import React from "react"
 import { createRoot } from "react-dom/client";
 import App from "./App";
-import "./application.css";
 
 document.addEventListener("DOMContentLoaded", () => {
   const root = document.getElementById("root");
